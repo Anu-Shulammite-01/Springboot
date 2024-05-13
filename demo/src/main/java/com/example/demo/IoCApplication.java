aa29314ee6aa8b692c884a420d38c39eda0b8ff1
Anu-Shulammite-01 <147792764+Anu-Shulammite-01@users.noreply.github.com>
2024-05-13 21:12:13 +0530

    Add files via upload
    Ioc Example using Dependency Injection

---
+++ demo/src/main/java/com/example/demo/IoCApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IoCApplication.class, args);
+		System.out.println("This is an example for IoC with Dependency Injection!");
+	}
+}
